fa1af82e545b11c4818c2960e80d1e3e98a17374
nrennie <nrennie35@gmail.com>
2023-11-20 20:50:46 +0000

    remove dependency on rgeos

--- R/geom_voronoi.R
+++ R/geom_voronoi.R
@@ -8,36 +8,39 @@
 #' @inheritParams ggplot2::geom_point
 #' @param outline \code{data.frame} with first column x/longitude, second column y/latitude, and a group column denoting islands or pieces.
 #' @keywords voronoi, choropleth
-#' @import ggplot2 sp deldir rgeos raster
+#' @import ggplot2 sp deldir raster
 #' @export
 #' @examples
 #' set.seed(45056)
-#' x <- sample(1:200,100)
-#' y <- sample(1:200,100)
+#' x <- sample(1:200, 100)
+#' y <- sample(1:200, 100)
 #' points <- data.frame(x, y,
-#'                      distance = sqrt((x-100)^2 + (y-100)^2))
-#' circle <- data.frame(x = 100*(1+cos(seq(0, 2*pi, length.out = 2500))),
-#'                      y = 100*(1+sin(seq(0, 2*pi, length.out = 2500))),
-#'                      group = rep(1,2500))
+#'   distance = sqrt((x - 100)^2 + (y - 100)^2)
+#' )
+#' circle <- data.frame(
+#'   x = 100 * (1 + cos(seq(0, 2 * pi, length.out = 2500))),
+#'   y = 100 * (1 + sin(seq(0, 2 * pi, length.out = 2500))),
+#'   group = rep(1, 2500)
+#' )
 #'
 #' ggplot(points) +
-#'     geom_voronoi(aes(x=x,y=y,fill=distance))
+#'   geom_voronoi(aes(x = x, y = y, fill = distance))
 #'
 #' ggplot(points) +
-#'     geom_voronoi(aes(x=x,y=y,fill=distance),outline=circle)
+#'   geom_voronoi(aes(x = x, y = y, fill = distance), outline = circle)
 #'
 #' ggplot(points) +
-#'     geom_voronoi(aes(x=x,y=y,fill=distance),fill=NA,color="black")
-
-geom_voronoi = function (mapping = NULL, data = NULL, stat = StatVoronoi, position = "identity",
-                         ...,na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, outline = NULL)
-{
-  if(is.null(mapping)){
-    mapping = aes(group=NA)
-  }else{
-    mapping$group=NA
+#'   geom_voronoi(aes(x = x, y = y, fill = distance), fill = NA, color = "black")
+geom_voronoi <- function(mapping = NULL, data = NULL, stat = StatVoronoi, position = "identity",
+                         ..., na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, outline = NULL) {
+  if (is.null(mapping)) {
+    mapping <- aes(group = NA)
+  } else {
+    mapping$group <- NA
   }
-  layer(data = data, mapping = mapping, stat = stat, geom = GeomPolygon,
-        position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-        params = list(na.rm = na.rm, outline = outline,...))
+  layer(
+    data = data, mapping = mapping, stat = stat, geom = GeomPolygon,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, outline = outline, ...)
+  )
 }
